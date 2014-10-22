ba4cf3546c7486ba8fef0022e737c0dbf0d544c5
TheRF <TheRF@users.noreply.github.com>
2014-10-23 00:41:50 +0200

    Update palcreate.py

--- palcreate.py
+++ palcreate.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from random import randint
 from palettegenerator import Palette
 
 DEBUG = True
 
-def debug(string):
+#------------------
+
+def debug(string, inte):
     '''debuging function
     arguments:
             string = message that should be displayed
     '''
     if DEBUG:
-        print('[DEBUG]',string)
+        print('[DEBUG]',string, inte)
+
+#-------------------
 
 def get_empty_palette(amount):
     '''get a palette full of empty (black) spaces
@@ -21,6 +25,8 @@ def get_empty_palette(amount):
             list of color tuples
     '''
     return [(0, 0 ,0) for i in range(0, amount)]
+
+#-------------------
 
 def get_random_colors(amount):
     '''we create a list of random colors
@@ -32,6 +38,8 @@ def get_random_colors(amount):
     if amount <= 0: return []
     
     return [(randint(0, 255), randint(0, 255), randint(0, 255)) for i in range(0, amount)]
+
+#-----------------
 
 # FIXME we need a better rounding function for more accurate conversions
 def convert_hls_rgb(hls):
@@ -109,6 +117,7 @@ def convert_hls_rgb(hls):
     
     return (r, g, b)
     
+#------------------
 
 def convert_rgb_hls(rgb):
     '''a given RGB color gets converted into hls
@@ -128,7 +137,7 @@ def convert_rgb_hls(rgb):
     
     # luminance value
     l = (round(((minval + maxval)/2)*100))/100
-    debug('lum: '+str(l))
+    debug('lum:', l)
     
     # saturation
     h = 0
@@ -138,9 +147,9 @@ def convert_rgb_hls(rgb):
     elif l < 0.5:
         s = round(((maxval-minval)/(maxval+minval))*100)/100
     else:
-        s = round(((maxval-minval)/(2-max-min))*100)/100
+        s = round(((maxval-minval)/(2-maxval-minval))*100)/100
         
-    debug('sat: '+str(s))
+    debug('sat:', s)
     # hue
     if fr == maxval:
         h = (fg-fb)/(maxval-minval)
@@ -153,10 +162,11 @@ def convert_rgb_hls(rgb):
     if h < 0:
         h += 360
         
-    debug('hue: '+str(h))
+    debug('hue:', h)
     
     return (h,l,s)
         
+#-------------------
 
 def get_palette_colors_rgb(amount, initcolor):
     '''create color steps in HLS
@@ -175,13 +185,95 @@ def get_palette_colors_rgb(amount, initcolor):
         col.append((col[i][0]-r, col[i][1]-g, col[i][2]-b))
         
     return col
-
+    
+#--------------------------
+    
+'''hue ranges'''
+RED = [355, 10]
+YELLOW = [51, 60]
+BLUE = [221, 240]
+# FIXME hue ranges for in-between colors
 def get_palette_colors_hls(amount, initcolor):
-    '''create color steps in HLS
+    '''create color ramps in HLS
     arguments:
             amount = number of colors we want in our list
-            initcolor = going from the lightest to the darkest color
+            initcolor = lightest color in RGB tuple
     returns:
             list of amount colors
     '''
-    pass
+    h, l, s = convert_rgb_hls(initcolor)
+    # first color -> transparent, second -> intial
+    colors = [(0,0,0)]
+    
+    # determine the starting color area depending on the hue
+    # 0 = red, 1 = yellow-red, 2 = yellow, 3 = yellow-blue, 4 = blue, 5 = blue-red
+    starthue = 0
+    if h <= RED[1] or h >= RED[0]:
+        starthue = 0
+    elif h > RED[1] and h < YELLOW[0]:
+        starthue = 1
+    elif h >= YELLOW[0] and h <= YELLOW[1]:
+        starthue = 2
+    elif h > YELLOW[1] and h < BLUE[0]:
+        starthue = 3
+    elif h >= BLUE[0] and h <= BLUE[1]:
+        starthue = 4
+    else:
+        starthue = 5
+    debug('starthue:', starthue)
+        
+    # we want darker colors, so determine difference between init hue and blue
+    mb = (BLUE[0] + BLUE[1])//2
+    diff = abs(h-mb)
+    debug('mb:', mb)
+    debug('difference:', diff)
+    
+    steps = round(diff/amount)
+    if h > mb:
+        steps *= -1
+    debug('steps:', steps)
+    
+    # we need a number for saturation and luminance
+    satdiff = s/25
+    lumdiff = l/16
+    
+    # get new colors
+    i = 0
+    hh = h + steps
+    ll = l + lumdiff
+    ss = s + satdiff
+    
+    hlslist = [(h,l,s)]
+    while i < amount:
+        # first in hls annotation
+        hh, ll, ss = hlslist[len(hlslist)-1][0]+steps, hlslist[len(hlslist)-1][1]-lumdiff, hlslist[len(hlslist)-1][2]+satdiff
+        if hh > 360:
+            hh -= 360
+        elif hh < 0:
+            hh += 360
+        
+        if ss > 1:
+            ss = 1
+        elif ss < 0:
+            ss = 0
+        
+        if ll > 1:
+            ll = 1
+        elif ll < 0:
+            ll = 0
+            
+        hlslist.append((hh, ll, ss))
+        i += 1
+    debug('list:',hlslist)
+    
+    # lastly convert hls colors back int rgb
+    for i in hlslist:
+        colors.append(convert_hls_rgb(i))
+        
+    cols = [colors[0]]
+    for i in reversed(colors[1:]):
+        cols.append(i)
+        
+    return cols
+
+#---------------------
